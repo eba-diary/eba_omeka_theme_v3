35b04a4e75106813b4e172c3e021c7d36c91b532
Chris Sumption <fridaysedge@gmail.com>
2016-05-25 08:06:18 -0700

    modify the header, footer, and index php files; will also make major changes to the css file; will add new image files

--- common/footer.php
+++ common/footer.php
@@ -1,7 +1,5 @@
 </div><!-- end content -->
-
 <footer>
-
     <div id="footer-content" class="center-div">
         <?php if($footerText = get_theme_option('Footer Text')): ?>
         <div id="custom-footer-text">
@@ -11,13 +9,25 @@
         <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
         <p><?php echo $copyright; ?></p>
         <?php endif; ?>
-        <p class="credits"><?php echo __('<a class="designed" href="http://emedara.com">Designed by Emedara Studio</a> <a class="powered" href="http://omeka.org">Proudly powered by Omeka</a>'); ?></p>
-
     </div><!-- end footer-content -->
-
-     <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
-
+    <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
+	<div class="row">
+		<div class="col-md-3">
+			<a title="Simpson Center For The Humanities" href="http://simpsoncenter.org/"><img src="http://www.emmabandrews.org/project/files/original/821ce75a8a9c11a8048f898ab3d02d0f.png" alt="" /></a>
+		</div>
+		<div class="col-md-3">
+			<a title="National Endowment For The Humanities" href="http://www.neh.gov/"><img src="http://www.emmabandrews.org/project/files/original/9de090726a76140d91b2166d3ca0c8b7.jpg" alt="" /></a>
+		</div>
+		<div class="col-md-3">
+			<a title="Department of Near Eastern Languages and Civilization" href="http://depts.washington.edu/nelc/"><img src="http://www.emmabandrews.org/project/files/original/c880eb6db6b6ae32d9cf48e751e11f63.jpg" alt="" /></a>
+		</div>
+		<div class="col-md-3">
+			<a title="Newbook Digital Texts" href="http://depts.washington.edu/ndth/"><img src="http://www.emmabandrews.org/project/files/original/35124865a943161413f0b5abe16ab7b1.png" alt="" /></a>
+		</div>
+	</div>
 </footer>
+
+
 
 <script type="text/javascript">
     jQuery(document).ready(function(){
@@ -26,6 +36,6 @@
     });
 </script>
 
+</div><!-- end container -->
 </body>
-
 </html>
--- common/header.php
+++ common/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <?php if ($description = option('description')): ?>
     <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>
@@ -33,20 +34,32 @@
 </head>
  <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
+	
         <header>
             <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
-            <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
+            <div id="site-title">
+				<?php echo link_to_home_page(theme_logo()); ?><br>
+				<a href="https://www.facebook.com/EmmaBAndrewsDiary?fref=ts"><img title="Facebook" src="http://www.emmabandrews.org/project/files/original/edaf2b410484e18b34270503d86436e1.png" alt="Facebook" /></a>&nbsp;
+				<a href="http://twitter.com/sarahketchley"><img title="Twitter" src="http://www.emmabandrews.org/project/files/original/993bea08bf0a00e3e7a8d1a9388d604b.png" alt="Twitter" /></a>&nbsp;
+				<a href="http://emmabandrews.tumblr.com/"><img title="Tumblr" src="http://www.emmabandrews.org/project/files/original/2d90b63e7fef5301933efd398ec41279.png" alt="Tumblr" /></a>
+			</div>
 			<div id="primary-nav">
-             <?php
-                  echo public_nav_main();
-             ?>
+				<?php echo public_nav_main();?>
+				<form class="form-inline" action="/project/search" method="get">
+					<input type="text" class="form-control" name="query" id="query" value="" title="Search" placeholder="">
+					<button class="btn btn-primary" name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
+				</form>
 			</div>
         </header>         
   
          <div id="mobile-nav">
-             <?php
-                  echo public_nav_main();
-             ?>
+            <p><?php echo public_nav_main();?></p>
+			<form class="form-inline" action="/project/search" method="get">
+				<div class="input-group">
+					<span class="input-group-addon" id="basic-addon1"><img src="http://www.emmabandrews.org/project/files/original/dc49ccca47590458afb61ac7db49868b.png" height="20" alt="search icon"/></span>
+					<input type="text" class="form-control" name="query" id="query" value="" title="Search" placeholder="">
+				</div>
+			</form>
          </div>
         
 		<?php if ((get_theme_option('Header Image') !== null)): ?>
@@ -63,8 +76,8 @@
 		</div>
 		<?php endif; ?>
 		
-		
-                       
+
+	<div class="container">                     
     <div id="content">
 
 <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
